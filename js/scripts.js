a25e7045336a628b0bfbe7c0a9c05b54f38f097a
tincho1980 <miramallo@gmail.com>
2026-04-04 14:01:41 -0300

    change px to em and rem sistem

--- js/scripts.js
+++ js/scripts.js
@@ -154,13 +154,13 @@
 
   document.querySelectorAll('a, button, .pdot, #hamburger, .proj-card, .proj-nav, .pill, .lang-switch a').forEach(function (el) {
     el.addEventListener('mouseenter', function () {
-      cur.style.width  = '16px';
-      cur.style.height = '16px';
+      cur.style.width  = '1.6rem';
+      cur.style.height = '1.6rem';
       curR.style.transform = 'translate(-50%,-50%) scale(1.5)';
     });
     el.addEventListener('mouseleave', function () {
-      cur.style.width  = '10px';
-      cur.style.height = '10px';
+      cur.style.width  = '1rem';
+      cur.style.height = '1rem';
       curR.style.transform = 'translate(-50%,-50%) scale(1)';
     });
   });
